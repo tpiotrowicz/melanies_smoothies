3e40f8a5533bbe97eb9bdcd024c2037704a2a642
Tom Piotrowicz <78644818+tpiotrowicz@users.noreply.github.com>
2025-07-16 05:28:17 -0400

    Update streamlit_app.py
    adding api call

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 # Import python packages
 import streamlit as st
-
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
