82750b110ba3340c1de096440bb1ea1aeb72e075
Tom Piotrowicz <78644818+tpiotrowicz@users.noreply.github.com>
2025-07-16 06:02:52 -0400

    updated display

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,13 +24,16 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 #st.dataframe(data=my_dataframe,use_container_width=True)
 ingredients_list = st.multiselect("Choose up to 5 ingredients:", my_dataframe, max_selections=5)
 
-smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
+
 if ingredients_list:
    ingredients_string = ''
    for fruit_chosen in ingredients_list:
        ingredients_string += fruit_chosen + ' '
+       smoothiefroot_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_chosen}")
+       sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
+
    time_to_insert = st.button("Submit Order")
    if time_to_insert: 
        my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order )
