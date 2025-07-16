a631ee6a8a3a58a5eec276109ac6d1f2aec6e3e5
Tom Piotrowicz <78644818+tpiotrowicz@users.noreply.github.com>
2025-07-16 06:04:09 -0400

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ if ingredients_list:
    ingredients_string = ''
    for fruit_chosen in ingredients_list:
        ingredients_string += fruit_chosen + ' '
-       smoothiefroot_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_chosen}")
+       smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_chosen")
        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 
